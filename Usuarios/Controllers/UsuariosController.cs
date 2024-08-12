5a9ef5ce3b30d167acbc67c3ed52077c666c2ba8
Anthony Muñoz <anthonymo577@gmail.com>
2024-08-11 20:05:20 -0400

    Agregar archivos de proyecto.

---
+++ Usuarios/Controllers/UsuariosController.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace Usuarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private string connection = @"Server=localhost; Database=usu; Uid=root; Password=root;";
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Models.usuario> lst = null;
+            using (var db = new MySqlConnection(connection))
+            {
+
+                var sql = "select usuarioPK,Nombre,Rut,Edad from usu.usuarios";
+
+                lst = db.Query<Models.usuario>(sql);
+
+            }
+
+            return Ok(lst);
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Models.usuario model)
+        {
+            string result = "Usuario ingresado";
+            using (var db = new MySqlConnection(connection))
+            {
+                var sql = "insert into usu.usuarios (Nombre, Rut, Edad) values (@Nombre, @Rut, @Edad)";
+                db.Execute(sql, new { Nombre = model.Nombre, Rut = model.Rut, Edad = model.Edad });
+            }
+            return Ok(result);
+
+        }
+
+        [HttpPut]
+        public IActionResult Edit(Models.usuario model)
+        {
+            string result = "Usuario editado";
+            using (var db = new MySqlConnection(connection))
+            {
+                var sql = "update usu.usuarios set Nombre=@Nombre, Rut=@Rut, Edad=@Edad where usuarioPK=@usuarioPK";
+                db.Execute(sql, new { usuarioPK = model.usuarioPK, Nombre = model.Nombre, Rut = model.Rut, Edad = model.Edad });
+            }
+            return Ok(result);
+
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Models.usuario model)
+        {
+            string result = "Usuario eliminado";
+            using (var db = new MySqlConnection(connection))
+            {
+                var sql = "delete from usu.usuarios where usuarioPK=@usuarioPK";
+                db.Execute(sql, model);
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ Usuarios/Models/usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace Usuarios.Models
+{
+    public class usuario
+    {
+        public int usuarioPK { get; set; }
+        public string Nombre { get; set; }
+        public string Rut { get; set; }
+
+        public int Edad {  get; set; }
+    }
+}
